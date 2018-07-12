0f9be1570b0b1f02b972fe3c46f35725f2c0a8e2
Ryan Wilson <r@trusona.com>
2018-07-11 17:11:03 -0700

    Cleaned build scripts and setting up Travis

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,8 +5,8 @@ cp data/settings.xml ~/.m2/settings.xml
 
 cat <<EOF > ~/.npmrc
 @trusona:registry=https://trusona.jfrog.io/trusona/api/npm/npm
-//trusona.jfrog.io/trusona/api/npm/:_password="$(echo -ne $artifactory_password | base64 -w 0)"
-//trusona.jfrog.io/trusona/api/npm/:username=$artifactory_username
-//trusona.jfrog.io/trusona/api/npm/:email=$artifactory_email
+//trusona.jfrog.io/trusona/api/npm/:_password="$(echo -ne $ARTIFACTORY_PASSWORD | base64 -w 0)"
+//trusona.jfrog.io/trusona/api/npm/:username=$ARTIFACTORY_USERNAME
+//trusona.jfrog.io/trusona/api/npm/:email=$ARTIFACTORY_EMAIL
 //trusona.jfrog.io/trusona/api/npm/:always-auth=true
 EOF
