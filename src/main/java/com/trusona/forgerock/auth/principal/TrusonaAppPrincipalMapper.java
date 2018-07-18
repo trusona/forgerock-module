df435f82aeddf2fd5ebdfe7ea3839083bc01b183
Nikolas Thitu <nikolas@hauz.io>
2018-07-18 00:40:48 -0400

    adding logging

--- src/main/java/com/trusona/forgerock/auth/principal/TrusonaAppPrincipalMapper.java
+++ src/main/java/com/trusona/forgerock/auth/principal/TrusonaAppPrincipalMapper.java
@@ -4,6 +4,7 @@ import com.sun.identity.authentication.internal.AuthPrincipal;
 import com.sun.identity.idm.AMIdentity;
 import com.trusona.client.TrusonaClient;
 import com.trusona.client.dto.response.UserResponse;
+import com.trusona.forgerock.auth.TrusonaDebug;
 import com.trusona.sdk.resources.dto.TrusonaficationResult;
 
 import java.security.Principal;
@@ -23,13 +24,17 @@ public class TrusonaAppPrincipalMapper implements PrincipalMapper {
   @Override
   public Optional<Principal> mapPrincipal(TrusonaficationResult result) {
     String trusonaId = result.getUserIdentifier().replace(TRUSONA_APP_PREFIX, "");
+    TrusonaDebug.getInstance().message("Looking up user by trusonaId {}", trusonaId);
+
     Optional<UserResponse> userResponse = trusonaClient.getUser(trusonaId);
 
     if (userResponse.isPresent()) {
       for (String email : userResponse.get().getEmails()) {
+        TrusonaDebug.getInstance().message("Looking within ForgeRock for user with email '{}'", email);
         AMIdentity identity = identityFinder.findForgeRockUser(email);
 
         if (identity != null) {
+          TrusonaDebug.getInstance().message("User found!");
           return Optional.of(new AuthPrincipal(identity.getUniversalId()));
         }
       }
@@ -37,6 +42,7 @@ public class TrusonaAppPrincipalMapper implements PrincipalMapper {
       return Optional.of(new AuthPrincipal(userResponse.get().getEmails().iterator().next()));
     }
     else {
+      TrusonaDebug.getInstance().warning("Did not find user by trusonaId {}", trusonaId);
       return Optional.empty();
     }
   }
