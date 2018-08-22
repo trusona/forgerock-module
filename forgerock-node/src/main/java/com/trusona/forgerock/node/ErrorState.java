8a0f915a1b9098e893fd4865a5b10d165f9abd05
Tom Piscitell <tom@trusona.com>
2018-08-22 11:47:08 -0400

    remove unused constructor

--- forgerock-node/src/main/java/com/trusona/forgerock/node/ErrorState.java
+++ forgerock-node/src/main/java/com/trusona/forgerock/node/ErrorState.java
@@ -12,30 +12,20 @@ import static com.trusona.forgerock.node.TrusonaOutcomes.ERROR_OUTCOME;
 public class ErrorState implements Supplier<Action> {
   private final String error;
   private final Debug debug;
-  private final Throwable throwable;
 
 
-  public ErrorState(String error, Debug debug, Throwable throwable) {
+  public ErrorState(String error, Debug debug) {
     this.error = error;
     this.debug = debug;
-    this.throwable = throwable;
   }
 
   public ErrorState(String error) {
-    this(error, TrusonaDebug.getInstance(), null);
-  }
-
-  public ErrorState(String error, Debug debug) {
-    this(error, debug, null);
-  }
-
-  public ErrorState(String error, Throwable throwable) {
-    this(error, TrusonaDebug.getInstance(), throwable);
+    this(error, TrusonaDebug.getInstance());
   }
 
   @Override
   public Action get() {
-    debug.message("In ErrorState", throwable);
+    debug.message("In ErrorState");
     if (StringUtils.isNotBlank(error)) {
       debug.error(error);
       return Action.goTo(ERROR_OUTCOME.id).build();
